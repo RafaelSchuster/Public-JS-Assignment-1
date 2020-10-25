1c8094825c2279e23432824428a234c6381aca1a
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-10-25 20:53:24 +0200

    2Mile Commit

--- js.js
+++ js.js
@@ -1,13 +1,17 @@
 let initial = ''
-let total = ''
+let total = [0,1]
 
+console.log(total)
 const number = document.getElementsByClassName('boxform')
 const result = document.getElementsByClassName('result')
 
-function fibonacci(initial,total){
-
-    document.querySelector('.form-control').value= initial
-    document.querySelector('.result').innerText = total
+function fibonacci(initial){
+    for(let i =2; i < 51; i++){
+        total[i] = total[i-1] + total[i-2]
+    }
+   
+     document.querySelector('.form-control').value= initial
+     document.querySelector('.result').innerText = total[initial]
     
 }
-fibonacci(3,8)
+fibonacci(14)
