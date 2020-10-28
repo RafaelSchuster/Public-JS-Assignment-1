074081ff0ed009a6fa613bd54fbb4c9c958e2a02
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-10-28 18:36:29 +0200

    5Milestone

--- js.js
+++ js.js
@@ -1,22 +1,56 @@
 let initial;
+let data;
 
 const number = document.querySelector('.boxform')
 const result = document.querySelector('.result')
 const buton = document.querySelector('.buton')
-
+const spin = document.querySelector('.spin')
+const alert = document.querySelector('.morethan')
+const error = document.querySelector('.error')
 
 const getData = () => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `http://localhost:5050/fibonacci/${initial}`);
     xhr.onload = () => {
-        const data = JSON.parse(xhr.response)
-        result.innerText = data.result
+        data = []
+        result.innerText = []
+        spin.className = 'spinner-border spin'
+        if (initial == 42) {
+            data = xhr.response
+            setTimeout(() => {
+                spin.className = 'spin'
+                result.innerText = `Server error: ${data}`
+                result.className = 'result errormsg'
+            }, 100)
+        }
+
+         else {
+            data = JSON.parse(xhr.response)
+            setTimeout(() => {
+                spin.className = 'spin'
+                result.innerText = data.result
+            }, 100)
+        }
     }
-    xhr.send();
+    xhr.send()
+
 }
 buton.addEventListener('click', () => {
-    initial = number.value - 1
-    getData()
-    return initial
-
+    initial = 0
+    if (number.value <= 50 && number.value > 0) {
+        alert.className = "morethan"
+        alert.innerText = ""
+        initial = number.value
+        getData()
+        return initial
+    }
+    if (number.value <= 0) {
+        result.innerText = []
+        alert.innerText = "Can't be less than 1"
+        alert.className = "alert alert-danger morethan"
+        return
+    } else {
+        alert.innerText = "Can't be larger than 50"
+        alert.className = "alert alert-danger morethan"
+    }
 })
