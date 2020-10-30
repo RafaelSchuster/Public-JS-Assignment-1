6d4a3ac2c0bb907d8e46516adaf1fdff1046f733
RAFAEL SCHUSTER <rsc1335@gmail.com>
2020-10-30 13:42:09 +0200

    WIP Milestone6-Friday

--- js.js
+++ js.js
@@ -1,6 +1,9 @@
 let initial;
 let data;
 let records;
+let initnum = [document.querySelector('.initialnum1'), document.querySelector('.initialnum2'), document.querySelector('.initialnum3')]
+let resnum = [document.querySelector('.resultnum1'), document.querySelector('.resultnum2'), document.querySelector('.resultnum3')]
+let datenum = [document.querySelector('.date1'), document.querySelector('.date2'), document.querySelector('.date3')]
 
 const number = document.querySelector('.boxform');
 const result = document.querySelector('.result');
@@ -8,6 +11,11 @@ const buton = document.querySelector('.buton');
 const spin = document.querySelector('.spin');
 const alert = document.querySelector('.morethan');
 const error = document.querySelector('.error');
+const previous = document.querySelector('.previous')
+const titleresults = document.querySelector('.titleresults')
+// const initialnum = document.querySelector('.initialnum1')
+// const resultnum = document.querySelector('.resultnum')
+// const datefib = document.querySelector('.date')
 
 const getData = () => {
     const xhr = new XMLHttpRequest();
@@ -54,16 +62,28 @@ buton.addEventListener('click', () => {
     };
 });
 
-window.addEventListener('load', function recording(){
+let datearray = []
+
+window.addEventListener('load', function recording() {
     const xhr2 = new XMLHttpRequest();
     xhr2.open('GET', `http://localhost:5050/getFibonacciResults`);
     xhr2.onload = () => {
-        spin.className = 'spinner-border spin';
+        spin.className = 'spinner-border spin2';
         let data2 = JSON.parse(xhr2.response)
-            setTimeout(() => {
-                spin.className = 'spin';
-                result.innerText = `The Fibonacci of ${data2.results[1].number} is ${data2.results[1].result} at `;
-            }, 100);
+        setTimeout(() => {
+            spin.className = 'spin';
+            for (let j = 0; j < 3; j++) {
+                datearray[j] = new Date(Number(`/Date(${data2.results[j].createdDate})/`.replace(/\D/g, '')))
+                console.log(datearray)
+                datenum[j].innerText = `\xa0${datearray[j]}`
+            }
+            for (let i = 0; i < 3; i++) {
+                // console.log(date)
+                initnum[i].innerText = `\xa0${data2.results[i].number}\xa0`;
+                resnum[i].innerText = `\xa0${data2.results[i].result}.\xa0`;
+            }
+        }, 100);
+
         console.log(data2);
     };
     xhr2.send();
